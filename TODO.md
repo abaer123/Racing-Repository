ed4d3a7dc4c39cdb4f65a8bf3cbc2b4394c49b72
lstucker <lstucker@gitlab.com>
2023-09-12 10:59:21 -0500

    beta

--- TODO.md
+++ TODO.md
@@ -6,9 +6,16 @@ do we dockerize it?
 - add a fuzz example, can take from what we have
 - add the sql injection part
 - maybe need a docker file?
+- need to fix table at some point
 
 Notes
 
 Run bundle lock --add-platform x86_64-linux when build breaks
 
 by default bundle exec rackup is ran for ruby applications
+
+
+To view database:
+
+sqlite3 leaderboard.db
+select * from leaderboard;
--- app.rb
+++ app.rb
@@ -5,36 +5,77 @@ require 'sqlite3'
 # database = Sequel.sqlite('development.sqlite')
 database = SQLite3::Database.open "leaderboard.db"
 database.execute "CREATE TABLE IF NOT EXISTS leaderboard(id primary_key, player varchar, time varchar, map varchar, date varchar)"
-
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Logan Stucker', '477', 'Woodland Whirlwind Run', '2023-02-21'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'John Doe', '122', 'Canyon Chase Challenge', '2023-02-23'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Sarah Johnson', '321', 'Rainforest Rally Run', '2023-03-01'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Michael Smith', '236', 'Urban Jungle Grand Prix', '2023-03-07'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Emily Davis', '745', 'Tundra Turbo Trails', '2023-04-27'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Daniel Wilson', '233', 'Mountain Mayhem Course', '2023-05-11'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Olivia Brown', '333', 'Desert Dash Circuit', '2023-05-14'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'James Lee', '521', 'Tundra Turbo Trails', '2023-07-01'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'William Martin', '511', 'Jungle Jamboree Track', '2023-07-07'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Benjamin White', '426', 'Ocean Odyssey Speedway', '2023-08-28'
+database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'Sophia Anderson', '499', 'Desert Dash Circuit', '2023-09-12'
 # TODO: primary key not being added by default
-# dbname.execute 'INSERT INTO leaderboard (player, time, date) VALUES (?, ?)', address, asking_price
-database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', 'logan', '15:35', 'troll tunnel', '12/12/2012'
+# dbname.execute 'INSERT INTO leaderboard (player, time, date) VALUES (?, ?)', 'address', 'asking_price'
 
-#  reference: https://github.com/al-kivi/puresong/blob/master/views/home.erb
-results = database.query "SELECT * FROM leaderboard"
 
-# results = database.execute("SELECT * FROM leaderboard").flatten
-test = results.first
-puts test
-puts test.instance_of? Array
-puts test.instance_of? Fixnum
-puts test.instance_of? String
-
+# <% results.each do |lp|  %>
+#   <% lp.each do |x|  %>
+#       <%= x %>
+#   <%  end  %>
+#   |||
+#   <%  end  %>
 
 # puts '{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}'
 # results.each do |i|
-#     i.each do |x|
-#         puts x
-#         puts '-'
-#     end
+#   puts i[1]
+#   puts i[2]
+#   puts i[3]
+#   puts i[4]
 # end
 # puts '{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}'
 
 get '/' do
+  @title = "Tanuki Racing Home"
+  erb :index, :locals => {}
+end
+
+get '/leaderboard' do
   @title = "Tanuki Racing Leaderboard"
-  erb :index, :locals => {:results => results}
+  results = database.query "SELECT * FROM leaderboard"
+  erb :leaderboard, :locals => {:results => results}
+end
+
+get '/edit' do
+  @title = "Tanuki Racing Edit"
+  erb :edit, :locals => {}
+end
+
+post '/edit_board' do
+  request_body = JSON.parse(request.body.read)
+  player = request_body['player']
+  time = request_body['time']
+  map = request_body['map']
+  date = request_body['date']
+
+  # add_entry(player, time, map, date)
+  @title = "Tanuki Racing Edit"
+  database.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', player, time, map, date
+  # results = database.query "SELECT * FROM leaderboard"
+  # erb :leaderboard, :locals => {:results => results}
+  redirect '/leaderboard'
+end
+
+get '/about' do
+  @title = "Tanuki Racing About"
+  erb :about, :locals => {}
 end
 
 not_found do
   erb :not_found
+end
+
+def add_entry(player, time, map, date)
+  database.execute "INSERT INTO leaderboard (player, time, map, date) VALUES (?,?,?,?)", player, time, map, date
 end
