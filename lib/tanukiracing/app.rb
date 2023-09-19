f4ffd4b0535138c011bf3b0c6a68a5e5ad53d4f0
lstucker <lstucker@gitlab.com>
2023-09-19 10:40:58 -0500

    db refactor

--- lib/tanukiracing/app.rb
+++ lib/tanukiracing/app.rb
@@ -2,7 +2,7 @@
 require 'sinatra'
 require 'sequel'
 require 'sqlite3'
-
+require_relative 'db'
 
 # frozen_string_literal: true
 
@@ -18,41 +18,19 @@ module TanukiRacing
 
     aws_key_id = "AKIAIOSF0DNN7EXAMPLE"
 
-    # database = Sequel.sqlite('development.sqlite')
-    database = SQLite3::Database.open "leaderboard.db"
-    # database.execute "SELECT * FROM leaderboard WHERE player = " + player_test
-    database.execute "CREATE TABLE IF NOT EXISTS leaderboard(id primary_key, player varchar, time varchar, map varchar, date varchar)"
-    query = 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)'
-    database.execute query, 'Logan Stucker', '477', 'Woodland Whirlwind Run', '2023-02-21'
-    database.execute query, 'John Doe', '122', 'Canyon Chase Challenge', '2023-02-23'
-    database.execute query, 'Sarah Johnson', '321', 'Rainforest Rally Run', '2023-03-01'
-    database.execute query, 'Michael Smith', '236', 'Urban Jungle Grand Prix', '2023-03-07'
-    database.execute query, 'Emily Davis', '745', 'Tundra Turbo Trails', '2023-04-27'
-    database.execute query, 'Daniel Wilson', '233', 'Mountain Mayhem Course', '2023-05-11'
-    database.execute query, 'Olivia Brown', '333', 'Desert Dash Circuit', '2023-05-14'
-    database.execute query, 'James Lee', '521', 'Tundra Turbo Trails', '2023-07-01'
-    database.execute query, 'William Martin', '511', 'Jungle Jamboree Track', '2023-07-07'
-    database.execute query, 'Benjamin White', '426', 'Ocean Odyssey Speedway', '2023-08-28'
-    database.execute query, 'Sophia Anderson', '499', 'Desert Dash Circuit', '2023-09-12'
-    # TODO: primary key not being added by default
-    # dbname.execute 'INSERT INTO leaderboard (player, time, date) VALUES (?, ?)', 'address', 'asking_price'
-
-
-    # <% results.each do |lp|  %>
-    #   <% lp.each do |x|  %>
-    #       <%= x %>
-    #   <%  end  %>
-    #   |||
-    #   <%  end  %>
-
-    # puts '{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}'
-    # results.each do |i|
-    #   puts i[1]
-    #   puts i[2]
-    #   puts i[3]
-    #   puts i[4]
-    # end
-    # puts '{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}'
+    TanukiRacing::DB.initialize("leaderboard.db")
+    TanukiRacing::DB.create_table
+    TanukiRacing::DB.add_leader('Logan Stucker', '477', 'Woodland Whirlwind Run', '2023-02-21')
+    TanukiRacing::DB.add_leader('John Doe', '122', 'Canyon Chase Challenge', '2023-02-23')
+    TanukiRacing::DB.add_leader('Sarah Johnson', '321', 'Rainforest Rally Run', '2023-03-01')
+    TanukiRacing::DB.add_leader('Michael Smith', '236', 'Urban Jungle Grand Prix', '2023-03-07')
+    TanukiRacing::DB.add_leader('Emily Davis', '745', 'Tundra Turbo Trails', '2023-04-27')
+    TanukiRacing::DB.add_leader('Daniel Wilson', '233', 'Mountain Mayhem Course', '2023-05-11')
+    TanukiRacing::DB.add_leader('Olivia Brown', '333', 'Desert Dash Circuit', '2023-05-14')
+    TanukiRacing::DB.add_leader('James Lee', '521', 'Tundra Turbo Trails', '2023-07-01')
+    TanukiRacing::DB.add_leader('William Martin', '511', 'Jungle Jamboree Track', '2023-07-07')
+    TanukiRacing::DB.add_leader('Benjamin White', '426', 'Ocean Odyssey Speedway', '2023-08-28')
+    TanukiRacing::DB.add_leader('Sophia Anderson', '499', 'Desert Dash Circuit', '2023-09-12')
 
     get '/' do
       @title = "Tanuki Racing Home"
@@ -61,7 +39,7 @@ module TanukiRacing
 
     get '/leaderboard' do
       @title = "Tanuki Racing Leaderboard"
-      results = database.query "SELECT * FROM leaderboard"
+      results = TanukiRacing::DB.get_all
       erb :leaderboard, :locals => {:results => results}
     end
 
@@ -78,10 +56,7 @@ module TanukiRacing
       date = request_body['date']
 
       @title = "Tanuki Racing Edit"
-      database.execute( "SELECT * FROM leaderboard WHERE player = ( #{player} )" )
-      # database.execute( "INSERT INTO Products ( stockID, Name ) VALUES ( #{id}, '#{name}' )" )
-      # database.execute "SELECT * FROM leaderboard WHERE player = " + request_body
-      database.execute("INSERT INTO leaderboard (player, time, map, date) VALUES (#{player}, #{time}, #{map}, #{date})")
+      TanukiRacing::DB.add_leader(player, time, map, date)
       redirect '/leaderboard'
     end
 
@@ -93,11 +68,6 @@ module TanukiRacing
     not_found do
       erb :not_found
     end
-
-    def add_entry(player, time, map, date)
-      database.execute "INSERT INTO leaderboard (player, time, map, date) VALUES (?,?,?,?)", player, time, map, date
-    end
-
 
   end
 end
--- lib/tanukiracing/db.rb
+++ lib/tanukiracing/db.rb
@@ -3,23 +3,27 @@ require 'sqlite3'
 
 module TanukiRacing
   class DB
-    def initialize(filename)
+    def self.initialize(filename)
       @filename = filename
-      @db = SQLite3::Database.new(filename)
+      @db = SQLite3::Database.open(filename)
     end
 
-    def create_table
+    def self.create_table
       @db.execute <<-SQL
       CREATE TABLE IF NOT EXISTS leaderboard(id primary_key, player varchar, time varchar, map varchar, date varchar)
       SQL
     end
 
-    def add_leader(player, time, map, date)
-      @db.execute("INSERT INTO leaderboard (player, time, map, date) VALUES (#{player}, #{time}, #{map}, #{date})")
+    def self.add_leader(player, time, map, date)
+        @db.execute 'INSERT INTO leaderboard (player, time, map, date) VALUES (?, ?, ?, ?)', player, time, map, date
     end
 
-    def get_leaders(id)
+    def self.get_leaders(id)
       @db.execute("SELECT * FROM leaders AND id = #{id}")
+    end
+
+    def self.get_all
+        @db.execute("SELECT * FROM leaderboard")
     end
 
     def delete_player(id)
