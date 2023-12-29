ec3541599e96600be4c655a13239f9f8cc587ef4
Paul Dumaitre <pdumaitre@gitlab.com>
2023-12-29 14:48:41 +0000

    add secrets to be found by secret detection

---
+++ Secrets/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Discord_Public_Key = "e7322523fb86ed64c836a979cf8465fbd436378c653c1db38f9ae87bc62a6fd5"
+const npmToken = "npm_JEbsYXyMwnGPcqZwk6fXplQNjmDHuJ0zoriU"
+
+func helloworld() string {
+	return "Hello World!!"
+}
+
+func main() {
+	fmt.Println(helloworld())
+}
---
+++ Secrets/secrets.md
@@ -0,0 +1,2 @@
+Secrets in this folder are intentionnally leaked to illustrate Secret Detection.
+They do not grant access to any resource or system.
